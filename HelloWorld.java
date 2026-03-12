16bc4fa7f9ffa237e3c6910d4d40fbf60876084d
Ponagalo <seroganyanemathaba@gmail.com>
2026-03-12 22:37:28 +0200

    First Commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	String myName = "Ponagalo Mathaba";
+	public static void main(String args ){
+		HelloWorld name = new HelloWorld();
+		System.out.println("Hello" + name.myName + ",Welcome to coding in DevOps");
+ }
+
+}
